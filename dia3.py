98faccdf0931e7734b7ada6c2716c00c48f33dfd
David Tintinapon <davidtintinapon.dev@gmail.com>
2026-05-30 01:16:40 -0500

    dia 3: variables y tipo de datos

---
+++ dia3.py
@@ -0,0 +1,24 @@
+nombre = "David"
+carrera = "Ingenieria de Sistemas"
+
+edad = 21
+ciclo = 5
+
+promedio = 15.5
+
+estudiando = True
+trabajando = False
+
+habilidades = ["Python", "Git", "VSCode"]
+
+perfil = {"nombre": "David", "edad": 21, "carrera": "Ing de Sistemas"}
+
+print(f"Nombre: {nombre}")
+print(f"Carrera: {carrera}")
+print(f"Edad: {edad}")
+print(f"Ciclo: {ciclo}")
+print(f"Promedio: {promedio}")
+print(f"Estudiando: {estudiando}")
+print(f"Trabajando: {trabajando}")
+print(f"Habilidades: {habilidades}")
+print(f"Perfil: {perfil}")
